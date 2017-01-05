b8047a6c04a75c03422e382447fa05e89f686c73
KEVIN J. CARAVAGGIO <caravk@uw.edu>
2017-01-04 18:07:29 -0800

    account activity for UI

--- src/main/webapp/script/user.js
+++ src/main/webapp/script/user.js
@@ -1,3 +1,6 @@
+var showErrorMessage = true;
+var errorMessage = "username or password is invalid"
+
 $(function() {
     $('#clearall').on('click', function() {
     	$('#username').val("");
@@ -19,7 +22,7 @@ $(function() {
     		url: "api/user/login",
     		method: "POST",
     		contentType: "application/json",
-    		data: userAuth,
+    		data: JSON.stringify(userAuth),
     		dataType: "json",
     		success: function(userInfo) {
     			var userInfoSerial = JSON.stringify(userInfo);
@@ -27,6 +30,51 @@ $(function() {
     		},
     		fail: function() {
     			alert("Error- ");
+    		}
+    	});
+    });
+});
+
+$(function() {
+    $('#register').on('click', function() {
+    	var failure = function() {
+    		if (showErrorMessage) {
+    			showErrorMessage = false;
+    			var blurb = $('#intro').html();
+    			$('#intro').html(blurb 
+    					+ "<br /><br />" 
+    					+ "<i>" 
+    					+ errorMessage 
+    					+ "</i>");
+    		}
+    	};
+    	
+    	var usernameValue = $('#username').val();
+    	var passwordValue = $('#password').val();
+    	
+    	if (!usernameValue || !passwordValue) {
+    		failure();
+    		return;
+    	}
+    	
+    	$('#username').val("");
+    	$('#password').val("");
+    	
+    	var userAuth = {username: usernameValue,
+    			password: passwordValue };
+    	
+    	$.ajax({
+    		url: "api/user/register",
+    		method: "POST",
+    		contentType: "application/json",
+    		data: JSON.stringify(userAuth),
+    		dataType: "json",
+    		success: function(userInfo) {
+    			var userInfoSerial = JSON.stringify(userInfo);
+    			sessionStorage.setItem("user",userInfoSerial);
+    		},
+    		fail: function() {
+    			failure();
     		}
     	});
     });
