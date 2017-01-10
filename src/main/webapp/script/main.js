2dd160d30c1c86ce2d1056b7c92933e276ba38e6
kevin <fortherecord686@gmail.com>
2017-01-09 17:47:16 -0800

    add contacts

--- src/main/webapp/script/main.js
+++ src/main/webapp/script/main.js
@@ -10,3 +10,47 @@ $( document ).ready(function() {
 		window.location = "./index.html"
 	}
 });
+
+$(function() {
+  $('#searchButton').on('click', function() {
+	var otherUser = $('#searchField').val();
+	if (!otherUser) {
+	  return;
+	}
+	
+	var conversation = { user: userToken.username, other: otherUser };
+	showUser(conversation.other, true);
+	$.ajax({
+		  url: "api/conv/exists",
+		  method: "POST",
+		  contentType: "application/json",
+		  data: JSON.stringify(conversation),
+		  dataType: "json",
+		  success: function(response) {
+			  showUser(conversation.other, response.exists);
+		  },
+		  fail: function() {
+			  showUser(conversation.other, false);
+		  }
+	});
+  });
+});
+
+function showUser(userName, exists) {
+  var div = document.getElementById('searchDiv');
+ 
+  var notifMessage = document.createElement('p');
+  notifMessage.setAttribute('class','notif');
+  notifMessage.onclick = function() {
+	div.removeChild(notifMessage);
+  };
+
+  if (exists) {
+	notifMessage.innerHTML = (userName + " added");
+  } else {
+	notifMessage.innerHTML = ("user not found");
+  }
+  setTimeout(notifMessage.onclick, 4000);
+
+  div.appendChild(notifMessage);
+}
